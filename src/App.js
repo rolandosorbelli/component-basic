c899f9dc97dc0ab5f89c6ef78658783300f3a5b3
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-02 17:57:37 +0000

    Used props to pass info from a Component to another

--- src/App.js
+++ src/App.js
@@ -3,23 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.yourName = "Panda"
-    this.state = {}
-  }
-
-  sayHello(name){
-    return "Hello " + name
-  }
-
-
   render() {
-    const myName = "Rolando"
     return (
       <div className="App">
-        <h2>Playing around with components: {this.yourName}</h2>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Parent />
+      </div>
+    );
+  }
+}
+
+class Parent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Just some info</h2>
+        <Child msg="I am a message in the parent" number = "123456" animals = {this.props.animals} />
+      </div>
+    );
+  }
+}
+
+Parent.defaultProps = {
+  animals: ["lion", "cat", "dog"]
+}
+
+class Child extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h3>I am from child component</h3>
+        <p>{this.props.msg}</p>
+        <p>{this.props.number}</p>
+        <p>{this.props.animals.map((item, i) => {
+          return " "+ item
+        })}</p>
       </div>
     );
   }
